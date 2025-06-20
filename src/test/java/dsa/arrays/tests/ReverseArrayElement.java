d00b1fcaeb7180591add72c92288f8c4fe5f1b89
Deepak.Ahlawat <ahlawat.qaengineer@gmail.com>
2025-06-20 22:49:21 +0530

    my reverse array

---
+++ src/test/java/dsa/arrays/tests/ReverseArrayElement.java
@@ -0,0 +1,45 @@
+package dsa.arrays.tests;
+
+public class ReverseArrayElement {
+
+
+
+
+    public  static void main (String [] args){
+
+        String [] inputArrayList = {"Arunachal" , "Assam", "Bengal" , "Bihar", "Chattisgarh", "Haryana", "Rajasthan" , "Sikkim", "Tripura", "TamilNadu"};
+        String [] outputArray = new String[10];
+        outputArray = reverseTheArray(inputArrayList);
+
+        for(String abc : outputArray){
+            System.out.println(abc);
+        }
+
+
+    }
+
+public static String[] reverseTheArray(String[] input){
+
+        int l = input.length;
+        String tempString = null;
+
+        for(int i = 0;  i<(input.length)/2;){
+          tempString = input[i];
+          input[i] = input[l-i-1];
+          input[l-i-1] = tempString;
+
+
+
+           i++;
+        }
+
+    return input;
+
+
+}
+
+
+
+
+
+}
